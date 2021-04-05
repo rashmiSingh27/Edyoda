3e3206bcd936738394b6391497300da09aabc3c5
rashmiSingh27 <79527908+rashmiSingh27@users.noreply.github.com>
2021-04-05 23:29:29 +0530

    Update practice.py
    Q1-Print all sub arrays for a given array. Ex - if array is [2, 7, 5],
    Output will be
    [2] [2, 7] [2, 7, 5] [7] [7, 5] [5]
    Q2-rotate the array for a given array.Ex-arr=[1, 4, 6, 8, 7],Output will be = [7, 1, 4, 6, 8]

--- DSA_assignments/practice.py
+++ DSA_assignments/practice.py
@@ -21,59 +21,12 @@ if __name__ == '__main__':
     B=[1, 4, 6, 8, 7]# [7, 1, 4, 6, 8]
 
 
-    #findSublists(A)
+    findSublists(A)
     rotate(B, len(B))
 
 
-class Node:
-    def __init__(self, data=None, next=None):
-        self.data = data
-        self.next = next
 
 
-class LL:
-    def __init__(self):
-        self.head = None
-
-    def append1(self, data):
-        new_node = Node(data)
-
-        if self.head is None:
-            self.head = new_node
-            return
-        temp = self.head
-        while (temp.next is not None):
-            temp = temp.next
-        temp.next = new_node
-    def append(self, new_data):
-        new_node = Node(new_data)
-
-        if self.head is None:
-            self.head = new_node
-            return
-
-        last = self.head
-        while (last.next is not None):
-            last = last.next
-
-        last.next = new_node
-
-    def printLL(self):
-        t= self.head
-
-        while t:
-            print(t.data, end="->>")
-            t = t.next
-    def rotateLL(self):
-        current = self.head
-        while (current):
-            print(current.data, end='=>')
-            current = current.next
 
 
-a = LL()
-a.append(1)
-a.append(2)
-a.append(3)
-a.append(4)
-a.printLL()
+
